896137170002dc0c9700f8c55291183effae8664
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-01 18:55:32 +0200

    feedScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,14 @@
-import { View, Text } from "react-native";
+import { FlatList } from "react-native";
+import PostListItem from "@/src/components/PostListItem";
+import posts from "@/assets/data/posts.json";
+
 export default function FeedScreen() {
   return (
-    <View>
-      <Text className="text-3xl font-bold">Feed</Text>
-    </View>
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <PostListItem post={item} />}
+      contentContainerStyle={{ gap: 10 }}
+      showsVerticalScrollIndicator={false}
+    />
   );
 }
