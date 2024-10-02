6e3a8058687511e85075c6f8105cf408fbed71e4
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-02 09:40:39 +0200

    image picker

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,20 +1,46 @@
 import { Text, View, Image, TextInput, Pressable } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 export default function CreatePost() {
   const [caption, setCaption] = useState<string>("");
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!image) {
+      pickImage();
+    }
+  }, [image]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
     <View className="p-3 items-center flex-1">
       {/* Image */}
-      <Image
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
-        }}
-        className="w-52 aspect-[3/4] rounded-lg "
-      />
+      {image ? (
+        <Image
+          source={{
+            uri: image,
+          }}
+          className="w-52 aspect-[3/4] rounded-lg bg-slate-300 "
+        />
+      ) : (
+        <View className="bg-slate-300 w-52 aspect-[3/4] rounded-lg " />
+      )}
 
-      <Text onPress={() => {}} className="text-blue-500 font-semibold m-5">
+      <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
         Change
       </Text>
 
